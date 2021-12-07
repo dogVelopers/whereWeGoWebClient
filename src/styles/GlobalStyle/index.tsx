b6ed8211a9b7dfa42780c525146acf0e8413c735
Hyesung Oh <haesungoh414@gmail.com>
2021-12-07 21:02:18 +0900

    [#2] apply global style

---
+++ src/styles/GlobalStyle/index.tsx
@@ -0,0 +1,13 @@
+import { css, Global } from '@emotion/react';
+
+export function GlobalStyle() {
+  return <Global styles={globalStyle} />;
+}
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
