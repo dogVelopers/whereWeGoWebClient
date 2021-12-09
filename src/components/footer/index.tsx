0eabf41df005c75eb6657e3090d787aa7aafd41a
Hyesung Oh <haesungoh414@gmail.com>
2021-12-10 00:02:29 +0900

    [#4] apply basic layout

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,13 @@
+import { css } from '@emotion/react';
+
 function Footer() {
-  return <footer>footer</footer>;
+  return <footer css={footerStyle}>footer</footer>;
 }
 
 export default Footer;
+
+const footerStyle = css`
+  width: 100%;
+  height: 30px;
+  background-color: lightgreen;
+`;
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,5 +1,13 @@
+import { css } from '@emotion/react';
+
 function Nav() {
-  return <nav>nav</nav>;
+  return <nav css={navStyle}>nav</nav>;
 }
 
 export default Nav;
+
+const navStyle = css`
+  width: 100%;
+  height: 60px;
+  background-color: green;
+`;
