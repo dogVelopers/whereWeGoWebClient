ef53f870f442da0a956367e9fd57c1bb11ab3223
Hyesung Oh <haesungoh414@gmail.com>
2021-12-09 22:47:29 +0900

    [#4] generate basic nav, main, footer components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import { css } from '@emotion/react';
 
+import Nav from 'components/nav';
+import Main from 'components/main';
+import Footer from 'components/footer';
+
 function Home() {
-  return <main css={wrapper}></main>;
+  return (
+    <div css={container}>
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
 
-const wrapper = css`
+const container = css`
   max-width: 428px;
   width: 100%;
   margin: 0 auto;
