80bbd3958e9ce832d60fdbff8f6152eca8b33f93
Hyesung Oh <haesungoh414@gmail.com>
2021-12-11 19:54:09 +0900

    [#7] apply sticky nav

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,12 +1,31 @@
+import Image from 'next/image';
 import { css } from '@emotion/react';
 
 function Nav() {
-  return <nav css={navStyle}>nav</nav>;
+  return (
+    <nav css={navStyle}>
+      <div css={logoWrapperStyle}></div>
+    </nav>
+  );
 }
 
 export default Nav;
 
 const navStyle = css`
+  position: sticky;
+  top: 0;
+
   width: 100%;
   height: 60px;
+  padding: 4px 6px;
+  border: solid 1px black;
+
+  display: flex;
+  align-items: center;
+`;
+
+const logoWrapperStyle = css`
+  width: 160px;
+  height: 100%;
+  background-color: green;
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ function Home() {
 export default Home;
 
 const container = css`
+  position: relative;
   max-width: 428px;
   width: 100%;
   margin: 0 auto;
