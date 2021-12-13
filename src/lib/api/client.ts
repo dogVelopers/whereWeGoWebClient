88ad206eea9322a23dad629083b5c8617db0ff0f
Hyesung Oh <haesungoh414@gmail.com>
2021-12-14 05:04:44 +0900

    [#9] apply response interceptor for axios instance

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,21 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const instance = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+
+const responseInterceptorFulfilled = (res: AxiosResponse) => {
+  if (200 <= res.status && res.status < 300) return res.data;
+
+  return Promise.reject(res.data);
+};
+
+const responseInterceptorRejected = (error: AxiosError) => {
+  return new Error(error.response?.data ?? error);
+};
+
+instance.interceptors.response.use(
+  responseInterceptorFulfilled,
+  responseInterceptorRejected
+);
 
 export function get<T>(...args: Parameters<typeof instance.get>) {
   return instance.get<T, T>(...args);
