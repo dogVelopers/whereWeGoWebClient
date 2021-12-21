4dae147e5f1b276391b014b3e20a5ac7328e1bb2
Hyesung Oh <haesungoh414@gmail.com>
2021-12-21 22:23:17 +0900

    [#21] apply ErrorBoundary

---
+++ src/components/common/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import { Component, ReactNode } from 'react';
+import ErrorPage from './ErrorPage';
+
+interface IErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface IState {
+  hasError: boolean;
+  errorMsg: string | null;
+}
+
+class ErrorBoundary extends Component<IErrorBoundaryProps, IState> {
+  state: IState = {
+    hasError: false,
+    errorMsg: null,
+  };
+
+  static getDerivedStateFromError(error: Error): IState {
+    return { hasError: true, errorMsg: error.message };
+  }
+
+  componentDidCatch(error: Error) {
+    console.error('Uncaught error:', error.message);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <ErrorPage
+          errorMsg={'문제가 발생하였습니다 지속될 시 문의 부탁드립니다'}
+        />
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,16 @@ import { css } from '@emotion/react';
 import Nav from 'components/nav';
 import Main from 'components/main';
 import Footer from 'components/footer';
+import ErrorBoundary from 'components/common/ErrorBoundary';
 
 function Home() {
   return (
     <div css={container}>
-      <Nav />
-      <Main />
-      <Footer />
+      <ErrorBoundary>
+        <Nav />
+        <Main />
+        <Footer />
+      </ErrorBoundary>
     </div>
   );
 }
