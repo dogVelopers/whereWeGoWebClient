053710d840f94bceae05b541ac48f9ddfabc5e59
Hyesung Oh <haesungoh414@gmail.com>
2022-01-02 22:18:11 +0900

    [#26] update _document setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,16 +43,15 @@ class WhereWeGoDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
           ></script>
-          <script>
-            dangerouslySetInnerHTML=
-            {{
+          <script
+            dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
             }}
-          </script>
+          />
         </Head>
         <body>
           <Main />
