411b579e7ec762ab62a1cf528823ac9b0073153b
Hyesung Oh <haesungoh414@gmail.com>
2022-01-02 22:10:00 +0900

    [#26] generate and apply useGaPageview at _app

---
+++ src/hooks/ga/useGaPageview.ts
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import { pageview } from 'lib/ga';
+import { useEffect } from 'react';
+
+function useGaPageview() {
+  const router = useRouter();
+  useEffect(() => {
+    function handleRouteChange() {}
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+}
+
+export default useGaPageview;
